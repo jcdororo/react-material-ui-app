d25b33837431cba6bcf48328982ed39a2d470aec
jc <wnwlcks123@gmail.com>
2023-10-02 16:40:42 +0900

    MUI 기본 사용법

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # MUI 설치
 `$ npm install @mui/material @emotion/react @emotion/styled`   
-`$ npm install @mui/icons-material`
+`$ npm install @mui/icons-material`   
+
+
+ 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button'
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import WorkspacesIcon from '@mui/icons-material/Workspaces';
+import styled from '@emotion/styled';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant='text'>Hello world</Button>
+      <Button startIcon={<WorkspacePremiumIcon />} fullWidth size='large' variant='contained'>Hello world</Button>
+      <Button endIcon={<WorkspacesIcon />} variant='outlined'>Hello world</Button>
+      <Button
+        sx={{
+          backgroundColor: 'gray',
+          color: 'white',
+          margin: 10,
+          "&:hover": {
+            backgroundColor: 'black'
+          },
+          "&:disabled": {
+            backgroundColor: 'white',
+            color: 'black'
+          }
+        }}
+
+      >Custom Style</Button>
+      <CustomButton>Custom Button Style</CustomButton>
+      <Typography variant='h1' gutterBottom>h1. Heading</Typography>
+      <Typography variant='h2' gutterBottom>h2. Heading</Typography>
+      <Typography variant='h3' gutterBottom>h3. Heading</Typography>
+
     </div>
   );
 }
+
+const CustomButton = styled(Button)({
+  backgroundColor: 'gray',
+          color: 'white',
+          margin: 10,
+          "&:hover": {
+            backgroundColor: 'black'
+          },
+          "&:disabled": {
+            backgroundColor: 'white',
+            color: 'black'
+          }
+})
 
 export default App;
