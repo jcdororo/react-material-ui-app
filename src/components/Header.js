0c5e77ba1e387a321e55ece79bfc5ac7d710ffc1
jc <wnwlcks123@gmail.com>
2023-10-03 17:40:49 +0900

    App Bar(Header)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import { AppBar, Toolbar, Typography, InputBase, Avatar } from '@mui/material'
+import styled from '@emotion/styled'
+import { AcUnit, Badge, Notifications } from '@mui/icons-material'
 
 const Header = () => {
+const StyledToolbar = styled(Toolbar) ({
+  display: 'flex',
+  justifyContent: 'space-between'
+
+})
+const Search = styled("div")(({theme})=>  ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%"
+}))
+
+const Icons = styled("div")(({theme}) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex"
+  }
+}))
+  
   return (
-    <div>Header</div>
+    <AppBar>
+      <StyledToolbar>
+        <Typography variant='h6' sx={{ display: { xs: "none", sm: "block" }}}>
+          MUI
+        </Typography>
+        <AcUnit sx={{ display: { xs: "block", sm: "none" }}} />
+        <Search>
+          <InputBase placeholder='search...' />
+        </Search>
+        <Icons>
+          <Badge badgeContent={3} color='error'>
+            <Notifications />
+          </Badge>
+          <Avatar sx={{ bgcolor: "gray" }}>N</Avatar>
+        </Icons>
+      </StyledToolbar>
+    </AppBar>
   )
 }
 
